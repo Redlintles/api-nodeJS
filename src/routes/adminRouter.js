ef7d1c2ca273c80322d5153c77d635bca6fdf922
redlintles <treelands54@gmail.com>
2024-05-11 22:04:58 -0300

    Adicionado endpoint /admin/add

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,4 +1,6 @@
 "use strict";
 var express = require("express");
 var router = express.Router();
+var createAdmin = require("../controllers/Admin/createAdmin");
+router.post("/add", createAdmin);
 module.exports = router;
--- src/routes/adminRouter.ts
+++ src/routes/adminRouter.ts
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const createAdmin = require("../controllers/Admin/createAdmin");
+
+router.post("/add", createAdmin);
 module.exports = router;
