62723e76361c0c35110deeedd1a611d2df279399
redlintles <treelands54@gmail.com>
2024-05-14 12:53:47 -0300

    Logging disabled for sequelize

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,13 +1,14 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE,
-    process.env.MYSQL_USER,
-    process.env.MYSQL_PASSWORD,
-    {
-        dialect: "mysql",
-        host: process.env.MYSQL_HOST
-    }
-)
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.MYSQL_HOST,
+    logging: false,
+  }
+);
 
-module.exports = sequelize
+module.exports = sequelize;
