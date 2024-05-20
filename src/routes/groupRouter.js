752ebce929ab3d5bc7c862bace44338970fd5189
redlintles <treelands54@gmail.com>
2024-05-20 14:58:35 -0300

    Adicionada configuração do multer

--- src/routes/groupRouter.js
+++ src/routes/groupRouter.js
@@ -2,5 +2,16 @@
 var express = require("express");
 var router = express.Router();
 var createGroup = require("../controllers/Group/createGroup");
-router.post("/add", createGroup);
+var multer = require("multer");
+var auth = require("../middlewares/auth");
+var upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: process.env.MAX_IMAGE_SIZE
+            ? parseInt(process.env.MAX_IMAGE_SIZE)
+            : 500000,
+    },
+});
+router.use(auth);
+router.post("/add", upload.single("banner"), createGroup);
 module.exports = router;
--- src/routes/groupRouter.ts
+++ src/routes/groupRouter.ts
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const createGroup = require("../controllers/Group/createGroup");
+const multer = require("multer");
+const auth = require("../middlewares/auth");
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: process.env.MAX_IMAGE_SIZE
+      ? parseInt(process.env.MAX_IMAGE_SIZE)
+      : 500000,
+  },
+});
 
-router.post("/add", createGroup);
+router.use(auth);
+router.post("/add", upload.single("banner"), createGroup);
 
 module.exports = router;
