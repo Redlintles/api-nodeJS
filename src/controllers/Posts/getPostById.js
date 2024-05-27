312afdb47642c1c26752d57fe557bbe4447747ff
redlintles <treelands54@gmail.com>
2024-05-27 16:01:56 -0300

    Adicionadas novas consultas em getPostById, que agora retorna todas as informações disponíveis sobre um post

--- src/controllers/Posts/getPostById.js
+++ src/controllers/Posts/getPostById.js
@@ -36,38 +36,61 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var Post = require("../../utils/models").Post;
+var _a = require("../../utils/models"), Post = _a.Post, PostLikes = _a.PostLikes, Comment = _a.Comment, sequelizeConn = _a.sequelizeConn;
 var validateId = require("../../utils/validateId");
 var getPostById = function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var id, userId, post;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
+    var id_post, postId, post, transaction, comments, likes, _a;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
             case 0:
-                id = req.query.id;
-                userId = validateId(id);
-                if (typeof userId === "string") {
+                id_post = req.query.id_post;
+                postId = validateId(id_post);
+                if (typeof postId === "string") {
                     return [2 /*return*/, res.status(400).json({
                             error: true,
-                            message: userId,
+                            message: postId,
                         })];
                 }
-                return [4 /*yield*/, Post.findByPk(userId)];
+                return [4 /*yield*/, Post.findByPk(postId)];
             case 1:
-                post = _a.sent();
+                post = _b.sent();
                 if (!post) {
                     return [2 /*return*/, res.status(400).json({
                             error: true,
                             message: "Post not found",
                         })];
                 }
-                else {
-                    return [2 /*return*/, res.status(200).json({
-                            error: false,
-                            message: "Post found successfully",
-                            post: post,
-                        })];
-                }
-                return [2 /*return*/];
+                return [4 /*yield*/, sequelizeConn.transaction()];
+            case 2:
+                transaction = _b.sent();
+                _b.label = 3;
+            case 3:
+                _b.trys.push([3, 6, , 7]);
+                return [4 /*yield*/, Comment.findAll({
+                        where: {
+                            id_post: postId,
+                        },
+                    })];
+            case 4:
+                comments = _b.sent();
+                return [4 /*yield*/, PostLikes.findAll({
+                        where: {
+                            id_post: postId,
+                        },
+                    })];
+            case 5:
+                likes = _b.sent();
+                return [2 /*return*/, res.status(200).json({
+                        error: false,
+                        message: "Post found successfully",
+                        obj: post,
+                        comments: comments,
+                        likes: likes.length,
+                    })];
+            case 6:
+                _a = _b.sent();
+                return [3 /*break*/, 7];
+            case 7: return [2 /*return*/];
         }
     });
 }); };
--- src/controllers/Posts/getPostById.ts
+++ src/controllers/Posts/getPostById.ts
@@ -1,35 +1,57 @@
 import { Request, Response } from "express";
 
-const { Post } = require("../../utils/models");
+const {
+  Post,
+  PostLikes,
+  Comment,
+  sequelizeConn,
+} = require("../../utils/models");
 
 const validateId = require("../../utils/validateId");
 
 const getPostById = async (req: Request, res: Response) => {
-  const { id } = req.query;
+  const { id_post } = req.query;
 
-  let userId = validateId(id);
+  let postId = validateId(id_post);
 
-  if (typeof userId === "string") {
+  if (typeof postId === "string") {
     return res.status(400).json({
       error: true,
-      message: userId,
+      message: postId,
     });
   }
 
-  const post = await Post.findByPk(userId);
+  const post = await Post.findByPk(postId);
 
   if (!post) {
     return res.status(400).json({
       error: true,
       message: "Post not found",
     });
-  } else {
+  }
+
+  const transaction = await sequelizeConn.transaction();
+  try {
+    const comments = await Comment.findAll({
+      where: {
+        id_post: postId,
+      },
+    });
+
+    const likes = await PostLikes.findAll({
+      where: {
+        id_post: postId,
+      },
+    });
+
     return res.status(200).json({
       error: false,
       message: "Post found successfully",
-      post,
+      obj: post,
+      comments,
+      likes: likes.length,
     });
-  }
+  } catch {}
 };
 
 module.exports = getPostById;
