448b6d41776138ce25d58bea88a069309fe82494
redlintles <treelands54@gmail.com>
2024-05-28 15:23:33 -0300

    Adicionado middleware idValidator em profileRouter

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -2,6 +2,8 @@
 var express = require("express");
 var router = express.Router();
 var multer = require("multer");
+var idValidator = require("../middlewares/idValidator");
+var models = require("../utils/models");
 var upload = multer({
     storage: multer.memoryStorage(),
     limits: { fileSize: 500000 },
@@ -9,7 +11,12 @@ var upload = multer({
 var auth = require("../middlewares/auth");
 var editProfile = require("../controllers/Profile/editProfile");
 router.use(auth);
-router.post("/edit", upload.fields([
+router.put("/edit", idValidator([
+    {
+        fieldStr: "id_user",
+        fieldObj: models.User,
+    },
+]), upload.fields([
     { name: "banner", maxCount: 1 },
     { name: "profilePhoto", maxCount: 1 },
 ]), editProfile);
--- src/routes/profileRouter.ts
+++ src/routes/profileRouter.ts
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+
+const idValidator = require("../middlewares/idValidator");
+const models = require("../utils/models");
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: 500000 },
@@ -10,8 +13,14 @@ const auth = require("../middlewares/auth");
 const editProfile = require("../controllers/Profile/editProfile");
 
 router.use(auth);
-router.post(
+router.put(
   "/edit",
+  idValidator([
+    {
+      fieldStr: "id_user",
+      fieldObj: models.User,
+    },
+  ]),
   upload.fields([
     { name: "banner", maxCount: 1 },
     { name: "profilePhoto", maxCount: 1 },
