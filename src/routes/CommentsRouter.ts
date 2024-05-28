792fbb3e93107e4bd11ff57c6e399c6af4770c1e
redlintles <treelands54@gmail.com>
2024-05-28 11:26:30 -0300

    Adicionado middleware idValidator em todas as rotas de commentsRouter

--- src/routes/CommentsRouter.ts
+++ src/routes/CommentsRouter.ts
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
+const idValidator = require("../middlewares/idValidator");
+const models = require("../utils/models");
 
 const createComment = require("../controllers/Comments/createComment");
 const deleteCommentById = require("../controllers/Comments/deleteCommentById");
@@ -8,8 +10,65 @@ const getCommentById = require("../controllers/Comments/getCommentById");
 const editComment = require("../controllers/Comments/editComment");
 
 router.use(auth);
-router.post("/add", createComment);
-router.delete("/deleteById", deleteCommentById);
-router.get("/getById", getCommentById);
-router.put("/editById", editComment);
+router.post(
+  "/add",
+  idValidator([
+    {
+      fieldStr: "id_author",
+      tableField: "id",
+      fieldObj: models.User,
+    },
+    {
+      fieldStr: "id_post",
+      tableField: "id",
+      fieldObj: models.Post,
+    },
+  ]),
+  createComment
+);
+router.delete(
+  "/deleteById",
+  idValidator(
+    [
+      {
+        fieldStr: "id",
+        tableField: "id",
+        fieldObj: models.Comment,
+      },
+    ],
+    false,
+    true
+  ),
+  deleteCommentById
+);
+router.get(
+  "/getById",
+  idValidator(
+    [
+      {
+        fieldStr: "id",
+        tableField: "id",
+        fieldObj: models.Comment,
+      },
+    ],
+    false,
+    true
+  ),
+  getCommentById
+);
+router.put(
+  "/editById",
+  idValidator(
+    [
+      {
+        fieldStr: "id",
+        tableField: "id",
+        fieldObj: models.Comment,
+      },
+    ],
+    false,
+    true
+  ),
+  editComment
+);
 module.exports = router;
