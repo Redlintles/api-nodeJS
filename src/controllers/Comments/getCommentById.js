57b2e4ccad443683d31a61538abcc0821dce1eca
redlintles <treelands54@gmail.com>
2024-05-28 11:29:00 -0300

    Removido código desnecessário de getComment

--- src/controllers/Comments/getCommentById.js
+++ src/controllers/Comments/getCommentById.js
@@ -36,35 +36,22 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var validateId = require("../../utils/validateId");
 var Comment = require("../../utils/models").Comment;
 var getCommentById = function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var id, commentId, comment, answers;
+    var id, comment, answers;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 id = req.query.id;
-                commentId = validateId(id);
-                if (typeof commentId === "string") {
-                    return [2 /*return*/, res.status(400).json({
-                            error: true,
-                            message: commentId,
-                        })];
-                }
-                return [4 /*yield*/, Comment.findByPk(commentId)];
+                return [4 /*yield*/, Comment.findByPk(id)];
             case 1:
                 comment = _a.sent();
-                if (!!comment) return [3 /*break*/, 2];
-                return [2 /*return*/, res.status(400).json({
-                        error: true,
-                        message: "Comment not Found",
+                return [4 /*yield*/, Comment.findAll({
+                        where: {
+                            belongs_to: comment.id,
+                        },
                     })];
-            case 2: return [4 /*yield*/, Comment.findAll({
-                    where: {
-                        belongs_to: comment.id,
-                    },
-                })];
-            case 3:
+            case 2:
                 answers = _a.sent();
                 return [2 /*return*/, res.status(200).json({
                         error: false,
--- src/controllers/Comments/getCommentById.ts
+++ src/controllers/Comments/getCommentById.ts
@@ -1,41 +1,24 @@
 import { Request, Response } from "express";
 
-const validateId = require("../../utils/validateId");
 const { Comment } = require("../../utils/models");
 
 const getCommentById = async (req: Request, res: Response) => {
   let { id } = req.query;
 
-  let commentId = validateId(id);
+  const comment = await Comment.findByPk(id);
 
-  if (typeof commentId === "string") {
-    return res.status(400).json({
-      error: true,
-      message: commentId,
-    });
-  }
+  const answers = await Comment.findAll({
+    where: {
+      belongs_to: comment.id,
+    },
+  });
 
-  const comment = await Comment.findByPk(commentId);
-
-  if (!comment) {
-    return res.status(400).json({
-      error: true,
-      message: "Comment not Found",
-    });
-  } else {
-    const answers = await Comment.findAll({
-      where: {
-        belongs_to: comment.id,
-      },
-    });
-
-    return res.status(200).json({
-      error: false,
-      message: "Comment and answers Fetched Successfully",
-      comment,
-      answers,
-    });
-  }
+  return res.status(200).json({
+    error: false,
+    message: "Comment and answers Fetched Successfully",
+    comment,
+    answers,
+  });
 };
 
 module.exports = getCommentById;
