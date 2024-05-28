682d5a056f42e65df4118e3220bf5225e1af1f56
redlintles <treelands54@gmail.com>
2024-05-28 14:47:11 -0300

    Adicionada o middleware idValidator na maioria das rotas de groupRouter

--- src/routes/groupRouter.ts
+++ src/routes/groupRouter.ts
@@ -22,15 +22,108 @@ const getGroupsWithUser = require("../controllers/Group/getGroupsWithUser");
 const addMember = require("../controllers/Group/addMember");
 const deleteMember = require("../controllers/Group/deleteMember");
 
+const models = require("../utils/models");
+const idValidator = require("../middlewares/idValidator");
+
 router.use(auth);
-router.post("/add", upload.single("banner"), createGroup);
-router.get("/getUserGroups", getUserGroups);
+
 router.get("/getAllGroups", getAllGroups);
-router.get("/getGroupById", getGroupById);
-router.get("/getGroupsWithUser", getGroupsWithUser);
-router.put("/editGroup", upload.single("banner"), editGroup);
-router.delete("/delete", deleteGroup);
-router.post("/addMember", addMember);
-router.delete("/deleteMember", deleteMember);
+
+router.post(
+  "/add",
+  idValidator([
+    {
+      fieldStr: "admin_id",
+      fieldObj: models.User,
+    },
+  ]),
+  upload.single("banner"),
+  createGroup
+);
+router.get(
+  "/getUserGroups",
+  idValidator([
+    {
+      fieldStr: "id_user",
+      fieldObj: models.User,
+    },
+  ]),
+  getUserGroups
+);
+
+router.get(
+  "/getGroupById",
+  idValidator([
+    {
+      fieldStr: "id_group",
+      fieldObj: models.Group,
+    },
+  ]),
+  getGroupById
+);
+router.get(
+  "/getGroupsWithUser",
+  idValidator([
+    {
+      fieldStr: "id_user",
+      fieldObj: models.User,
+    },
+  ]),
+  getGroupsWithUser
+);
+router.put(
+  "/editGroup",
+  idValidator([
+    {
+      fieldStr: "group_id",
+      fieldObj: models.Group,
+    },
+    {
+      fieldStr: "admin_id",
+      fieldObj: models.User,
+    },
+  ]),
+  upload.single("banner"),
+  editGroup
+);
+router.delete(
+  "/delete",
+  idValidator([
+    {
+      fieldStr: "admin_id",
+      fieldObj: models.User,
+    },
+  ]),
+  deleteGroup
+);
+
+router.post(
+  "/addMember",
+  idValidator([
+    {
+      fieldStr: "id_user",
+      fieldObj: models.User,
+    },
+    {
+      fieldStr: "id_group",
+      fieldObj: models.Group,
+    },
+  ]),
+  addMember
+);
+router.delete(
+  "/deleteMember",
+  idValidator([
+    {
+      fieldStr: "id_member",
+      fieldObj: models.User,
+    },
+    {
+      fieldStr: "id_group",
+      fieldObj: models.Group,
+    },
+  ]),
+  deleteMember
+);
 
 module.exports = router;
