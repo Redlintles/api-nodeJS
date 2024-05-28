974a31f604284f096701fe168349c42b92441e54
redlintles <treelands54@gmail.com>
2024-05-28 15:14:36 -0300

    Removido código de validação de id em getPostsByUserId

--- src/controllers/Posts/getPostsByUserId.js
+++ src/controllers/Posts/getPostsByUserId.js
@@ -37,35 +37,18 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var Post = require("../../utils/models").Post;
-var User = require("../../utils/models").User;
-var validateId = require("../../utils/validateId");
 var getPostsByUserId = function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var id, userId, user, posts;
+    var id, posts;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 id = req.query.id;
-                userId = validateId(id);
-                if (typeof userId === "string") {
-                    return [2 /*return*/, res.status(400).json({
-                            error: true,
-                            message: userId,
-                        })];
-                }
-                return [4 /*yield*/, User.findByPk(userId)];
-            case 1:
-                user = _a.sent();
-                if (!!user) return [3 /*break*/, 2];
-                return [2 /*return*/, res.status(400).json({
-                        error: true,
-                        message: "Usuário não encontrado",
+                return [4 /*yield*/, Post.findAll({
+                        where: {
+                            id_author: id,
+                        },
                     })];
-            case 2: return [4 /*yield*/, Post.findAll({
-                    where: {
-                        id_author: userId,
-                    },
-                })];
-            case 3:
+            case 1:
                 posts = _a.sent();
                 return [2 /*return*/, res.status(200).json({
                         error: false,
--- src/controllers/Posts/getPostsByUserId.ts
+++ src/controllers/Posts/getPostsByUserId.ts
@@ -1,42 +1,21 @@
 import { Request, Response } from "express";
 
 const { Post } = require("../../utils/models");
-const { User } = require("../../utils/models");
-
-const validateId = require("../../utils/validateId");
 
 const getPostsByUserId = async (req: Request, res: Response) => {
   const { id } = req.query;
 
-  let userId = validateId(id);
+  const posts = await Post.findAll({
+    where: {
+      id_author: id,
+    },
+  });
 
-  if (typeof userId === "string") {
-    return res.status(400).json({
-      error: true,
-      message: userId,
-    });
-  }
-
-  const user = await User.findByPk(userId);
-
-  if (!user) {
-    return res.status(400).json({
-      error: true,
-      message: "Usuário não encontrado",
-    });
-  } else {
-    const posts = await Post.findAll({
-      where: {
-        id_author: userId,
-      },
-    });
-
-    return res.status(200).json({
-      error: false,
-      message: "Posts Encontrados com sucesso",
-      posts,
-    });
-  }
+  return res.status(200).json({
+    error: false,
+    message: "Posts Encontrados com sucesso",
+    posts,
+  });
 };
 
 module.exports = getPostsByUserId;
