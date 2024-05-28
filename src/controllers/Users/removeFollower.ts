e2d294b9dc5b5220190786848dc4da2ed4a090cf
redlintles <treelands54@gmail.com>
2024-05-28 16:02:22 -0300

    Removido código de validação de id em removeFollower

--- src/controllers/Users/removeFollower.ts
+++ src/controllers/Users/removeFollower.ts
@@ -1,47 +1,14 @@
 import { Request, Response } from "express";
 
-const validateId = require("../../utils/validateId");
-const { User, UserFollower } = require("../../utils/models");
+const { UserFollower } = require("../../utils/models");
 
 const removeFollower = async (req: Request, res: Response) => {
   const { id_follower, id_followed } = req.body;
 
-  const followerId = validateId(id_follower);
-  const followedId = validateId(id_followed);
-
-  if (typeof followedId === "string") {
-    return res.status(400).json({
-      error: true,
-      message: followedId,
-    });
-  }
-  if (typeof followerId === "string") {
-    return res.status(400).json({
-      error: true,
-      message: followerId,
-    });
-  }
-
-  const followed = await User.findByPk(followedId);
-  const follower = await User.findByPk(followerId);
-
-  if (!followed) {
-    return res.status(400).json({
-      error: true,
-      message: "Followed user doesn't exists",
-    });
-  }
-  if (!follower) {
-    return res.status(400).json({
-      error: true,
-      message: "Follower user doesn't exists",
-    });
-  }
-
   const alreadyExists = await UserFollower.findOne({
     where: {
-      id_followed: followedId,
-      id_follower: followerId,
+      id_followed,
+      id_follower,
     },
   });
 
