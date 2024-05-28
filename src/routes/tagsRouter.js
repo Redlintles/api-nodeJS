78ee1e87370529a34b96dfd557c96ed2e0d447b6
redlintles <treelands54@gmail.com>
2024-05-28 15:28:24 -0300

    Adicionado middleware de validação de id em tagsROuter

--- src/routes/tagsRouter.js
+++ src/routes/tagsRouter.js
@@ -4,7 +4,14 @@ var router = express.Router();
 var createTag = require("../controllers/Tags/createTag");
 var deleteTag = require("../controllers/Tags/deleteTag");
 var auth = require("../middlewares/auth");
+var idValidator = require("../middlewares/idValidator");
+var models = require("../utils/models");
 router.use(auth);
 router.post("/add", createTag);
-router.delete("/delete", deleteTag);
+router.delete("/delete", idValidator([
+    {
+        fieldStr: "id_tag",
+        fieldObj: models.Tag,
+    },
+]), deleteTag);
 module.exports = router;
--- src/routes/tagsRouter.ts
+++ src/routes/tagsRouter.ts
@@ -5,8 +5,20 @@ const createTag = require("../controllers/Tags/createTag");
 const deleteTag = require("../controllers/Tags/deleteTag");
 const auth = require("../middlewares/auth");
 
+const idValidator = require("../middlewares/idValidator");
+const models = require("../utils/models");
+
 router.use(auth);
 router.post("/add", createTag);
-router.delete("/delete", deleteTag);
+router.delete(
+  "/delete",
+  idValidator([
+    {
+      fieldStr: "id_tag",
+      fieldObj: models.Tag,
+    },
+  ]),
+  deleteTag
+);
 
 module.exports = router;
