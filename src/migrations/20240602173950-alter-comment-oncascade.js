7560e8058a564cc82aa24141eb0c53259b798ec8
redlintles <treelands54@gmail.com>
2024-06-02 14:47:12 -0300

    Corrigido erro em definição de oncascade no banco de dados

---
+++ src/migrations/20240602173950-alter-comment-oncascade.js
@@ -0,0 +1,73 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.changeColumn("Comments", "id_author", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    });
+
+    await queryInterface.changeColumn("Comments", "id_post", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Posts",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    });
+
+    await queryInterface.changeColumn("Comments", "belongs_to", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Comments",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.changeColumn("Comments", "id_author", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+    });
+
+    await queryInterface.changeColumn("Comments", "id_post", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Posts",
+        key: "id",
+      },
+    });
+
+    await queryInterface.changeColumn("Comments", "belongs_to", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Comments",
+        key: "id",
+      },
+    });
+  },
+};
